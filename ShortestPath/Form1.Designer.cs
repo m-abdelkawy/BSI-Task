cbbececae9c4fe265048272e19089e258c74cc22
M.Abdelkawy <engineer_m.abdelkawy@yahoo.com>
2019-08-26 07:34:36 +0200

    BSI Polygon no intersection

--- ShortestPath/Form1.Designer.cs
+++ ShortestPath/Form1.Designer.cs
@@ -28,12 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.pointBtn = new System.Windows.Forms.Button();
+            this.xCoorTxtBox = new System.Windows.Forms.TextBox();
+            this.yCoorTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DistanceLbl2 = new System.Windows.Forms.Label();
+            this.DistanceLbl1 = new System.Windows.Forms.Label();
+            this.destinationLbl = new System.Windows.Forms.Label();
+            this.SourceLbl = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(51, 352);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(100, 27);
+            this.drawBtn.TabIndex = 0;
+            this.drawBtn.Text = "Draw";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.drawBtn_Click);
+            // 
+            // pointBtn
+            // 
+            this.pointBtn.Location = new System.Drawing.Point(51, 127);
+            this.pointBtn.Name = "pointBtn";
+            this.pointBtn.Size = new System.Drawing.Size(100, 28);
+            this.pointBtn.TabIndex = 1;
+            this.pointBtn.Text = "Add Point";
+            this.pointBtn.UseVisualStyleBackColor = true;
+            this.pointBtn.Click += new System.EventHandler(this.pointBtn_Click);
+            // 
+            // xCoorTxtBox
+            // 
+            this.xCoorTxtBox.Location = new System.Drawing.Point(51, 37);
+            this.xCoorTxtBox.Name = "xCoorTxtBox";
+            this.xCoorTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.xCoorTxtBox.TabIndex = 2;
+            this.xCoorTxtBox.Text = "0";
+            this.xCoorTxtBox.TextChanged += new System.EventHandler(this.xCoorTxtBox_TextChanged);
+            // 
+            // yCoorTxtBox
+            // 
+            this.yCoorTxtBox.Location = new System.Drawing.Point(51, 81);
+            this.yCoorTxtBox.Name = "yCoorTxtBox";
+            this.yCoorTxtBox.Size = new System.Drawing.Size(100, 22);
+            this.yCoorTxtBox.TabIndex = 3;
+            this.yCoorTxtBox.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Y:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.DistanceLbl2);
+            this.panel1.Controls.Add(this.DistanceLbl1);
+            this.panel1.Controls.Add(this.destinationLbl);
+            this.panel1.Controls.Add(this.SourceLbl);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.drawBtn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pointBtn);
+            this.panel1.Controls.Add(this.yCoorTxtBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.xCoorTxtBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 689);
+            this.panel1.TabIndex = 6;
+            // 
+            // DistanceLbl2
+            // 
+            this.DistanceLbl2.AutoSize = true;
+            this.DistanceLbl2.Location = new System.Drawing.Point(128, 451);
+            this.DistanceLbl2.Name = "DistanceLbl2";
+            this.DistanceLbl2.Size = new System.Drawing.Size(0, 17);
+            this.DistanceLbl2.TabIndex = 11;
+            // 
+            // DistanceLbl1
+            // 
+            this.DistanceLbl1.AutoSize = true;
+            this.DistanceLbl1.Location = new System.Drawing.Point(12, 451);
+            this.DistanceLbl1.Name = "DistanceLbl1";
+            this.DistanceLbl1.Size = new System.Drawing.Size(63, 17);
+            this.DistanceLbl1.TabIndex = 10;
+            this.DistanceLbl1.Text = "Distance";
+            // 
+            // destinationLbl
+            // 
+            this.destinationLbl.AutoSize = true;
+            this.destinationLbl.Location = new System.Drawing.Point(3, 282);
+            this.destinationLbl.Name = "destinationLbl";
+            this.destinationLbl.Size = new System.Drawing.Size(116, 17);
+            this.destinationLbl.TabIndex = 9;
+            this.destinationLbl.Text = "Destination Index";
+            // 
+            // SourceLbl
+            // 
+            this.SourceLbl.AutoSize = true;
+            this.SourceLbl.Location = new System.Drawing.Point(3, 230);
+            this.SourceLbl.Name = "SourceLbl";
+            this.SourceLbl.Size = new System.Drawing.Size(90, 17);
+            this.SourceLbl.TabIndex = 8;
+            this.SourceLbl.Text = "Source Index";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 282);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 227);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.canvas.Location = new System.Drawing.Point(225, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(652, 689);
+            this.canvas.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(883, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 692);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1067, 689);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shortest Path";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.Button pointBtn;
+        private System.Windows.Forms.TextBox xCoorTxtBox;
+        private System.Windows.Forms.TextBox yCoorTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Label DistanceLbl2;
+        private System.Windows.Forms.Label DistanceLbl1;
+        private System.Windows.Forms.Label destinationLbl;
+        private System.Windows.Forms.Label SourceLbl;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- ShortestPath/Form1.cs
+++ ShortestPath/Form1.cs
@@ -12,9 +12,146 @@ namespace ShortestPath
 {
     public partial class Form1 : Form
     {
+        Pen redPen = new Pen(Color.Black, 3);
+        Graphics g = null;
+
+        public List<Point> points = new List<Point>();
+        double[] angle;
+
+        static int start_x, start_y;
+        
+
+        //Form Constructor
         public Form1()
         {
             InitializeComponent();
+            start_x = canvas.Width / 2;
+            start_y = canvas.Height / 2;
         }
+
+        private void drawBtn_Click(object sender, EventArgs e)
+        {
+            Point[] points2 = new Point[points.Count];
+            points2 = ArrangePoints(points.ToArray());
+
+            Point[] pointsClosed = new Point[points2.Length + 1];
+
+            for (int i = 0; i < points2.Length; i++)
+            {
+                pointsClosed[i] = points2[i];
+            }
+            pointsClosed[pointsClosed.Length - 1] = pointsClosed[0];
+
+            g = canvas.CreateGraphics();
+            drawLine(redPen, pointsClosed);
+
+            //canvas.Refresh();
+            
+
+        }
+        public void DrawPoints(Pen pen, Point p)
+        {
+            g.DrawRectangle(pen, p.X, p.Y, 10, 10);
+        }
+
+        int addBtnClicked = 0;
+        private void pointBtn_Click(object sender, EventArgs e)
+        {
+            g = canvas.CreateGraphics();
+
+            int x = Convert.ToInt32(xCoorTxtBox.Text);
+            int y = Convert.ToInt32(yCoorTxtBox.Text);
+            Point p = new Point(x, y);
+            DrawPoints(redPen, p);
+            points.Add(p);
+
+            start_x = canvas.Width / 2;
+            start_y = canvas.Height / 2;
+
+            listBox1.Items.Add($"P{addBtnClicked}- X: {p.X}, Y: {p.Y}");
+            addBtnClicked++;
+        }
+
+        public void drawLine(Pen pen, Point[] points)
+        {
+            g.DrawLines(pen, points);
+        }
+        public Point[] ArrangePoints(Point[] points)
+        {
+            int minX = points[0].X;
+            int maxX = points[0].X;
+            int minY = points[0].Y;
+            int maxY = points[0].Y;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (points[i].X < minX) minX = points[i].X;
+                if (points[i].X > maxX) maxX = points[i].X;
+                if (points[i].Y < minY) minY = points[i].Y;
+                if (points[i].Y > maxY) maxY = points[i].Y;
+            }
+
+            //central point
+            int xCenter = minX + (maxX - minX) / 2;
+            int yCenter = minY + (maxY - minY) / 2;
+            Point center = new Point(xCenter, yCenter);
+
+            //angle
+            angle = new double[points.Length];
+            for (int i = 0; i < points.Length; i++)
+            {
+                double numerator = Convert.ToDouble(points[i].X - center.X);
+
+                double denomenator = LineDistance(points[i], center);
+
+                angle[i] = Math.Acos(numerator / denomenator);
+                if (points[i].Y > center.Y)
+                {
+                    angle[i] = 2 * Math.PI - angle[i];
+                }
+            }
+            SortByAngle(angle, points);
+
+            return points;
+            
+        }
+        public double LineDistance(Point p1, Point p2)
+        {
+            double xDif = p1.X - p2.X;
+            double yDif = p1.Y - p2.Y;
+
+            double lineDist = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
+            return lineDist;
+        }
+
+        private void xCoorTxtBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SortByAngle(double[] angle, Point[] points)
+        {
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < points.Length - i - 1; j++)
+                {
+                    if (angle[j].CompareTo(angle[j+1]) > 0)
+                    {
+                        double tempAngle = angle[j];
+                        angle[j] = angle[j + 1];
+                        angle[j + 1] = tempAngle;
+
+                        Point tempPoint = points[j];
+                        points[j] = points[j + 1];
+                        points[j + 1] = tempPoint;
+
+                        swapped = true;
+                    }
+                }
+                if (!swapped) break;
+            }
+        }
+        
     }
 }
